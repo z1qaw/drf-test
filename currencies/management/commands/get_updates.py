16510e65ce1e7a0a83e6c8cbf659c6b6d95789ee
s1q <39670960+s1q@users.noreply.github.com>
2021-03-24 20:33:49 +0300

    Implement get_updates command for updating currencies in db

---
+++ currencies/management/commands/get_updates.py
@@ -0,0 +1,34 @@
+import xml.etree.ElementTree as ET
+
+import requests
+from django.core.management.base import BaseCommand
+
+from ...models import Currency
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        currs_info = get_updates()
+        for curr in currs_info:
+            Currency.objects.update_or_create(
+                name=curr['Name'], defaults={'rate': curr['Value']}
+            )
+        self.stdout.write(self.style.SUCCESS(
+            'Currency instances has been successfully updated.'
+            )
+        )
+
+
+def get_updates() -> list:
+    updates = requests.get('http://www.cbr.ru/scripts/XML_daily.asp').text
+    currs_list = ET.fromstring(updates).findall('Valute')
+    
+    updates = []
+    for c_el in currs_list:
+        result = {}
+        for item in list(c_el.iter())[1:]:
+            result[item.tag] = item.text
+        result['Value'] = float(result['Value'].replace(',', '.'))
+        updates.append(result)
+    
+    return updates
