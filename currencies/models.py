19e13fff3375769f4ec68dc2101dbd56e0c9e78c
s1q <39670960+s1q@users.noreply.github.com>
2021-03-24 20:37:00 +0300

    Add __repr__ and __str__ methods of Currency model

--- currencies/models.py
+++ currencies/models.py
@@ -5,6 +5,11 @@ class Currency(models.Model):
     name = models.CharField(verbose_name='Currency name', max_length=100, unique=True)
     rate = models.FloatField(verbose_name='Currency rate to Rouble')
 
+    def __repr__(self):
+        return f'{self.name}: {self.rate} Roubles'
+
+    def __str__(self):
+        return self.name
     class Meta:
         db_table = 'currency'
         verbose_name_plural = 'Currencies'
