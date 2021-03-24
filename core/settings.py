3e2bf53cd85997ad4a1443f09f6a53b6f70a40f2
s1q <39670960+s1q@users.noreply.github.com>
2021-03-24 03:21:16 +0300

    Add DRF pagination settings

--- core/settings.py
+++ core/settings.py
@@ -122,3 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 20,
+}
